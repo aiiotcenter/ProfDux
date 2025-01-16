6687edc78b933ebc79af9714ce77f24118064c33
Ibrahim Ame <ame.ibrahim@yahoo.com>
2025-01-16 12:13:46 +0200

    ... number updates

--- student/Exam.php
+++ student/Exam.php
@@ -9,7 +9,7 @@
 
     <?php include '../include/studentImports.php'; ?>
 
-    <script src="../js/TakeExamView.js?11"></script>
+    <script src="../js/TakeExamView.js?13"></script>
     
 </head>
 
